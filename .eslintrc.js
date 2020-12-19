6611ee8337dd84f71c99802b74406fdec7b703fb
biniruu <biniruu@gmail.com>
2020-12-19 15:41:45 +0900

    Set prettier and eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  env: {
+    node: true,
+  },
+  extends: [
+    'standard',
+    'plugin:vue/strongly-recommended',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'prettier/vue',
+  ],
+  // required to lint *.vue files
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': 'off',
+    'space-before-function-paren': 'off',
+    'no-console': 'off',
+    quotes: [
+      'error',
+      'single',
+      {
+        allowTemplateLiterals: true,
+      },
+    ],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    camelcase: [
+      'error',
+      {
+        properties: 'never',
+      },
+    ],
+    'prettier/prettier': 'warn',
+    'vue/v-bind-style': ['warn', 'shorthand'],
+    'vue/require-v-for-key': 'warn',
+    'new-cap': 'error',
+    'prefer-rest-params': 'error',
+    'no-new-object': 'error',
+    'no-array-constructor': 'error',
+    'no-duplicate-imports': 'error',
+    'no-inner-declarations': 'off',
+  },
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Setting up prettier & eslint
+
+토이 프로젝트나 학습 시 사용하는 개인 설정.
