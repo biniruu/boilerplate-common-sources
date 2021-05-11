ffdfd655132952404308070f065d56def30b8d06
biniruu <biniruu@gmail.com>
2021-05-11 21:57:45 +0900

    Update .stylelintrc

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,14 +3,99 @@ module.exports = {
   plugins: ['stylelint-scss', 'stylelint-order'],
   syntax: 'scss',
   rules: {
-    "indentation": 2,
-    "color-hex-case": "lower",
-    // 'at-rule-no-unknown': null,
-    // 'scss/at-rule-no-unknown': true,
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+      },
+    ],
+    'at-rule-name-case': 'lower',
+    'at-rule-name-space-after': 'always-single-line',
+    'at-rule-semicolon-newline-after': 'always',
+    'at-rule-semicolon-space-before': 'never',
+    'block-closing-brace-empty-line-before': 'never',
+    'block-closing-brace-newline-before': ['always'],
+    'block-opening-brace-newline-after': ['always'],
+    'color-hex-case': 'lower',
+    'color-hex-length': 'short',
+    'comment-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+      },
+    ],
+    'comment-whitespace-inside': 'always',
+    'custom-property-empty-line-before': [
+      'always',
+      {
+        except: ['after-custom-property', 'first-nested'],
+      },
+    ],
+    'declaration-bang-space-after': 'never',
+    'declaration-bang-space-before': 'always',
+    'declaration-colon-space-after': 'always',
+    'declaration-colon-space-before': 'never',
+    'declaration-empty-line-before': [
+      'always',
+      {
+        except: ['after-declaration', 'first-nested'],
+      },
+    ],
+    'font-family-name-quotes': 'always-where-recommended',
+    'function-comma-space-after': 'always',
+    'function-max-empty-lines': 0,
+    'function-name-case': 'lower',
+    'function-parentheses-newline-inside': 'always-multi-line',
+    'function-parentheses-space-inside': 'never',
+    'function-url-quotes': 'always',
+    'function-whitespace-after': 'always',
+    'selector-type-no-unknown': [
+      true,
+      {
+        ignore: ["custom-elements", "default-namespace"],
+        // ignoreNamespaces: ["/regex/", /regex/, "string"],
+        // ignoreTypes: ["/regex/", /regex/, "string"],
+      }
+    ],
+    'max-empty-lines': [
+      1,
+      {
+        ignore: ["comments"]
+      }
+    ],
+    'media-feature-colon-space-after': 'always',
+    'media-feature-colon-space-before': 'never',
+    'media-feature-name-case': 'lower',
+    'media-feature-parentheses-space-inside': 'never',
+    'media-query-list-comma-newline-after': 'never-multi-line',
+    'media-query-list-comma-newline-before': 'never-multi-line',
+    'number-leading-zero': 'always',
+    'number-max-precision': 4,
+    'number-no-trailing-zeros': true,
     'order/properties-alphabetical-order': true,
-    // 'no-empty-source': null,
-    // 'rule-empty-line-before': null,
-    // 'selector-list-comma-newline-after': null,
-    // 'no-descending-specificity': null
+    'property-case': 'lower',
+    'rule-empty-line-before': [
+      'always-multi-line',
+      {
+        except: ['after-single-line-comment', 'first-nested'],
+      },
+    ],
+    'selector-attribute-brackets-space-inside': 'never',
+    'selector-attribute-operator-space-after': 'never',
+    'selector-attribute-operator-space-before': 'never',
+    'selector-attribute-quotes': 'never',
+    'selector-combinator-space-after': 'always',
+    'selector-combinator-space-before': 'always',
+    'selector-descendant-combinator-no-non-space': true,
+    'selector-pseudo-class-case': 'lower',
+    'selector-pseudo-class-parentheses-space-inside': 'never',
+    'selector-pseudo-element-case': 'lower',
+    'selector-type-case': 'lower',
+    'selector-list-comma-newline-after': 'always-multi-line',
+    'string-quotes': 'single',
+    'unit-case': 'lower',
+    'value-keyword-case': 'lower',
+    'value-list-comma-space-after': 'always',
+    'value-list-comma-space-before': 'never',
   },
 }
