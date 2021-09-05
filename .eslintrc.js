5467c2792a5c064e80e52e7be8aa300b21c2cb9c
biniruu <biniruu@gmail.com>
2021-09-05 18:18:52 +0900

    chore: update package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
+  env: { browser: true, es6: true, node: true },
   extends: [
     'eslint-config-prettier',
     'eslint-config-sinon',
