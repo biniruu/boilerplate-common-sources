3e3fbd996fe05ba9b8844d00e7eb9b21361abf9d
biniruu <biniruu@gmail.com>
2022-10-11 23:46:42 +0900

    update stylelintrc

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  customSyntax: 'postcss-html', // postcss를 사용하는 환경에서 stylelint(CssSyntaxError) 에러 발생 방지
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   plugins: ['stylelint-scss', 'stylelint-order'],
   // ignoreFiles: ['./src/style.css'],
