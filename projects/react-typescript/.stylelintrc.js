58e78fd2936c253a406aa703f9860c1813c51ca1
biniruu <biniruu@gmail.com>
2023-04-30 17:38:34 +0900

    chore: remove unnecessary packages

--- projects/react-typescript/.stylelintrc.js
+++ projects/react-typescript/.stylelintrc.js
@@ -3,7 +3,6 @@ module.exports = {
   overrides: [
     {
       customSyntax: 'postcss-html', // postcss를 사용하는 환경에서 stylelint(CssSyntaxError) 에러 발생 방지
-      // customSyntax: '@stylelint/postcss-css-in-js',
       files: ['**/*.{html,jsx,svg,tsx}'],
     },
   ],
