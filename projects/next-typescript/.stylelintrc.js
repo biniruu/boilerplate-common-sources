43862afa43244546de521b22773a6cd16dfb1a79
biniruu <biniruu@gmail.com>
2023-05-09 01:36:13 +0900

    chore: remove unnecessary stylelintrc file from next-typescript folder

--- projects/next-typescript/.stylelintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: '../../.stylelintrc.js',
-  overrides: [
-    {
-      customSyntax: 'postcss-html', // postcss를 사용하는 환경에서 stylelint(CssSyntaxError) 에러 발생 방지
-      files: ['**/*.{html,jsx,svg,tsx}'],
-    },
-  ],
-}
