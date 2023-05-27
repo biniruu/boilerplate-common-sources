44696c4681c73752e66385ebc78b647859a6f6f2
biniruu <biniruu@gmail.com>
2023-05-27 14:23:05 +0900

    chore: update .eslintrc in vanillajs-typescript

--- vanillajs-typescript/.eslintrc.cjs
+++ vanillajs-typescript/.eslintrc.cjs
@@ -168,13 +168,23 @@ module.exports = {
      * order: import 자동 정렬. warnOnUnassignedImports는 항상 default값(false)로 놔둘 것. true로 할 경우 import 정렬 관련 경고가 발생하는데, 이 문제는 import/order 또는 sort-import 설정만으로는 해결 불가
      */
     'import/no-unresolved': 'off',
-    // 'import/newline-after-import': ['warn', { considerComments: true }],
-    // 'import/order': [
-    //   'warn',
-    //   {
-    //     alphabetize: { caseInsensitive: true, order: 'asc', orderImportKind: 'asc' },
-    //   },
-    // ],
+    'import/newline-after-import': [
+      'warn',
+      {
+        // considerComments: true, // 알 수 없는 이유로 사용 불가
+        count: 3,
+      },
+    ],
+    'import/order': [
+      'warn',
+      {
+        alphabetize: {
+          caseInsensitive: true,
+          order: 'asc',
+          // orderImportKind: 'asc', // 알 수 없는 이유로 사용 불가
+        },
+      },
+    ],
     /**
      * Eslint-plugin-jsx-a11y supported rules
      * {@link https://github.com/jsx-eslint/eslint-plugin-jsx-a11y#supported-rules}
