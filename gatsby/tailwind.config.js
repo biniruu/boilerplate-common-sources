8ab06903c584d790f5cdade41cef270fc14402a7
biniruu <biniruu@gmail.com>
2023-06-19 13:01:37 +0900

    chore: update some configuration files

--- gatsby/tailwind.config.js
+++ gatsby/tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    `./src/pages/**/*.{js,jsx,ts,tsx}`,
-    `./src/components/**/*.{js,jsx,ts,tsx}`,
-  ],
+  content: ['./src/*.{js,ts,jsx,tsx,mdx}', './src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {},
+      fontSize: {},
+      colors: {
+        green: '#11ac3a',
+        red: '#e33920',
+        yellow: '#dfb230',
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'),
+      }),
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
   },
   plugins: [],
 }
