56d75d44f8529367cc30082d6a7033ba40e7bcbb
biniruu <biniruu@gmail.com>
2023-08-07 20:07:52 +0900

    chore: set styledCompoennts to true

--- next-typescript/next.config.js
+++ next-typescript/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
-    // styledComponents: true, // styled component 활성화
+    styledComponents: true, // styled component 활성화
   },
   images: {
     domains: [], // 외부 이미지 uri
