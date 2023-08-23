883d09f61fb7fa44c589bb1c05539fc466664b4d
biniruu <biniruu@gmail.com>
2023-08-23 17:11:13 +0900

    chore: add serverActions options to next.config.js

--- next-typescript/next.config.js
+++ next-typescript/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   compiler: {
     styledComponents: true, // styled component 활성화
   },
+  // experimental: {
+  //   serverActions: true, // App Router에서 server actions 활성화
+  // },
   images: {
     domains: [], // 외부 이미지 uri
   },
