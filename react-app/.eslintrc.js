b069ab6c364b0bfe93055596c4ace5db2ed82852
biniruu <biniruu@gmail.com>
2023-10-07 00:36:35 +0900

    chore: update .eslintrc in react-app

--- react-app/.eslintrc.js
+++ react-app/.eslintrc.js
@@ -13,15 +13,27 @@ module.exports = {
    * Extends
    *
    * eslint:recommended : eslint 추천 rule set
+   * plugin:@tanstack/eslint-plugin-query/recommended : tanstack query (react query) 추천 rule set
    * plugin:import/recommended : eslint-plugin-import 추천 rule set
    * plugin:jsx-a11y/recommended : 웹 접근성 관련 추천 rule set
    * plugin:prettier/recommended : eslint-config-prettier 추천 rule set
+   * plugin:react-hooks/recommended
+   * plugin:react/jsx-runtime : If you are using the new JSX transform from React 17, you should enable this
+   * plugin:react/recommended
+   * plugin:storybook/recommended : 스토리북 추천 rule set
+   * react-app : eslint-config-react-app으로 eslint 설정 덮어쓰기
    */
   extends: [
     'eslint:recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
     'plugin:import/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react/recommended',
+    'plugin:storybook/recommended',
+    'react-app',
   ],
   overrides: [
     {
@@ -29,8 +41,9 @@ module.exports = {
        * Jest
        *
        * plugin:jest/recommended : eslint-plugin-jest 추천 rule set
+       * react-app/jest : jest 규칙 설정을 위한 eslint-config-react-app 확장
        */
-      extends: ['plugin:jest/recommended'],
+      extends: ['plugin:jest/recommended', 'react-app/jest'],
       files: ['*.spec.js', '*.test.js'],
       rules: {
         /**
@@ -41,7 +54,6 @@ module.exports = {
     },
   ],
   parser: '@babel/eslint-parser',
-  plugins: ['pug'],
   root: true, // 현재 설정 파일이 root임을 명시하는 옵션. true로 설정하면 상위 설정 파일 찾기를 여기서 멈춘다.
   rules: {
     /**
@@ -111,12 +123,7 @@ module.exports = {
     'no-new-object': 'warn',
     'no-undef': 'error',
     'no-underscore-dangle': 'error',
-    'no-unused-vars': [
-      'error',
-      {
-        args: 'all',
-      },
-    ],
+    'no-unused-vars': 'off',
     'no-useless-escape': 'warn',
     'no-var': 'error',
     'object-curly-spacing': ['warn', 'always'],
@@ -151,9 +158,9 @@ module.exports = {
      *
      * 공식 문서에서는 옵션 설정을 추천하지 않는다. prettier-vscode 확장 프로그램이 .prettierrc 파일을 읽고 이곳에 있는 옵션은 무시하는데, 이 때문에 예상치 못한 문제가 발생할 수도 있기 때문.
      *
-     * prettier : 이곳에 설정한 옵션은 .prettier 파일에 있는 옵션을 덮어쓴다.
+     * prettier : 이곳에 설정한 옵션은 .prettier 파일에 있는 옵션을 덮어쓴다. 타입스크립트에서는 알 수 없는 이유로 에러를 발생시키는 경우가 잦아 off로 설정해 두었다.
      */
-    'prettier/prettier': 'warn',
+    'prettier/prettier': 'off',
     /**
      * Eslint-plugin-import rules
      * {@link https://github.com/import-js/eslint-plugin-import#rules}
@@ -217,8 +224,90 @@ module.exports = {
         td: ['gridcell'],
       },
     ],
+    /**
+     * Eslint-plugin-react rules
+     * {@link https://github.com/jsx-eslint/eslint-plugin-react/tree/master/docs/rules}
+     *
+     * destructuring-assignment : state, prop 등에 구조분해 할당 적용
+     * jsx-curly-brace-presence : jsx 내 불필요한 중괄호 금지
+     * jsx-curly-spacing
+     * jsx-key : 반복문으로 생성하는 요소에 key 속성 강제. 'react/recommended' 설정 시 활성화
+     * jsx-no-useless-fragment : 불필요한 fragment 금지
+     * jsx-pascal-case : 컴포넌트 이름을 PascalCase로 강제
+     * jsx-no-bind : JSX에서 .bind() 또는 화살표 함수 사용 금지
+     * jsx-uses-react : react를 import한 후 JSX 사용 강제. 'react/recommended' 설정 시 활성화. 'no-unused-vars'가 활성화 된 경우 효과 발생. react v17 이후 필요없어짐 {@link https://legacy.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#eslint How to Upgrade to the New JSX Transform}
+     * jsx-uses-vars : JSX를 import한 후 해당 JSX 사용 강제. 'no-unused-vars'가 활성화 된 경우 효과 발생
+     * no-direct-mutation-state : state 직접 수정 금지. 'react/recommended' 설정 시 활성화
+     * no-unescaped-entities : JSX 안에서 escape 되지 않은 entity 코드 사용 금지. 'react/recommended' 설정 시 활성화
+     * no-unknown-property : DOM property에 해당하지 않는 property를 비활성화
+     * no-unused-state : 사용하지 않는 state가 있을 시 경고 발생
+     * prop-types : prop의 type을 정의하도록 강제. 'react/recommended' 설정 시 활성화. typescript를 사용하면 필요없는 옵션
+     * react-in-jsx-scope : component에서 React를 import하지 않을 경우 오류 발생. 'react/recommended' 설정 시 활성화. react v17 이후 필요없어짐 {@link https://legacy.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#eslint How to Upgrade to the New JSX Transform}
+     * self-closing-comp : JSX 태그 안에 하위 태그가 없을 경우 self-closing 태그로 변환
+     * static-property-placement : 클래스에서 childContextTypes, contextTypes, contextType, defaultProps, displayName, propTypes를 정의하도록 강제. default : 'static public field'
+     * react-hooks/rules-of-hooks : react hooks 공식 문서에서 제공하는 규칙을 준수하도록 강제. {@link https://legacy.reactjs.org/docs/hooks-rules.html Roles of Hooks}
+     * react-hooks/exhaustive-deps : useEffect 안에서 사용하는 함수나 변수를 dependency로 등록하지 않았을 때 경고 발생
+     */
+    'react/destructuring-assignment': 'warn',
+    'react/jsx-curly-brace-presence': 'warn',
+    // 'react/jsx-curly-spacing': ['warn', { when: 'always', children: true, objectLiterals: 'never' }], // prettier와 충돌하여 사용할 수 없음
+    'react/jsx-key': 'error',
+    'react/jsx-no-useless-fragment': [
+      'warn',
+      {
+        allowExpressions: true,
+      },
+    ],
+    'react/jsx-pascal-case': 'warn',
+    'react/jsx-no-bind': [
+      'error',
+      {
+        allowArrowFunctions: true,
+        allowFunctions: true,
+      },
+    ],
+    'react/jsx-uses-react': 'off',
+    'react/jsx-uses-vars': 'error',
+    'react/no-direct-mutation-state': 'error',
+    'react/no-unescaped-entities': 'error',
+    'react/no-unknown-property': [
+      'error',
+      {
+        ignore: ['jsx'],
+      },
+    ],
+    'react/no-unused-state': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': [
+      'warn',
+      {
+        component: true,
+        html: false,
+      },
+    ],
+    'react/static-property-placement': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      // {
+      //   additionHooks: '(useRecoilCallback|useRecoilTransaction_UNSTABLE)', // recoil 사용 시 필요
+      // },
+    ],
   },
   settings: {
+    /**
+     * Eslint-plugin-import resolvers
+     * {@link https://github.com/import-js/eslint-plugin-import#resolvers}
+     * {@link https://github.com/import-js/eslint-plugin-import#importextensions}
+     *
+     * resolver.node.extensions : require more granular extension definitions. jsx를 import할 때 import/no-unresolved 에러가 발생하지 않도록 함
+     */
+    'import/resolver': {
+      node: {
+        extensions: ['*.js', '*.jsx'],
+      },
+    },
     /**
      * Jest version setting
      * {@link https://github.com/jest-community/eslint-plugin-jest#jest-version-setting}
@@ -227,6 +316,15 @@ module.exports = {
      */
     jest: {
       version: require('jest/package.json').version,
+    },
+    /**
+     * Eslint-plugin-react configuration
+     * {@link https://github.com/jsx-eslint/eslint-plugin-react#configuration-legacy-eslintrc-}
+     *
+     * 'detect' automatically picks the version you have installed.
+     */
+    react: {
+      version: 'detect',
     },
   },
 }
