fa40b3d72fccb877c5c7367b6872b9a012f09211
biniruu <biniruu@gmail.com>
2024-03-21 15:32:49 +0900

    feat: update settings of webpack for typescript

--- typescript/ts/webpack.config.ts
+++ typescript/ts/webpack.config.ts
@@ -8,6 +8,7 @@ import path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import type { Configuration as WebpackConfiguration } from 'webpack'
+import { SourceMapDevToolPlugin } from 'webpack'
 import type { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server'
 import WorkboxWebpackPlugin from 'workbox-webpack-plugin'
 
@@ -20,8 +21,15 @@ const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader
 
 const config: Configuration = {
   devServer: {
+    // allowedHosts: [],
+    compress: true,
     open: true,
     host: 'localhost',
+    port: 8000,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    watchFiles: ['src/**/*.ts', 'public/*.html'],
   },
   entry: './src/main.ts',
   mode: isProduction ? 'production' : 'development',
@@ -50,6 +58,7 @@ const config: Configuration = {
     ],
   },
   output: {
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   /**
@@ -57,8 +66,9 @@ const config: Configuration = {
    */
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: './public/index.html',
     }),
+    new SourceMapDevToolPlugin({}),
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.jsx', '.js'],
